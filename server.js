a93014f30950d046d3685f88ed01bb93c36071f8
Cailin Young <cailinfayyoung@gmail.com>
2021-02-27 13:10:05 -0700

    adding heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
+console.log(process.env.MONGO_URI)
 
 mongoose.connect(
   process.env.MONGO_URI, {
